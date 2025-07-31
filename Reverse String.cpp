4c30bb183aa24c56389ae33237a080057c9abc70
akshitchordia <chordiaakshit3@gmail.com>
2025-07-31 10:41:34 +0530

    Create Reverse String.cpp

---
+++ Reverse String.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1;
+    string str2;
+    int n;
+cout << "Enter the string to reverse\n";
+cin >> str1;
+n = str1.length();
+cout << "Reversed String\n";
+for(int i = n;i>=0;i--){
+    cout << str1[i] ;
+}
+    return 0;
+}
+
+// OUTPUT
+// Enter the string to reverse
+// akshit
+// Reversed String
+// tihska
