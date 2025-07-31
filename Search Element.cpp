6dc342860635cb7ee7d39096edab078339cbabfe
akshitchordia <chordiaakshit3@gmail.com>
2025-07-31 10:38:46 +0530

    Create Search Element.cpp

---
+++ Search Element.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int key,i;
+    int x[5] = {34,56,78,90,20};
+    cout << "Enter the number to find: ";
+    cin >> key;
+   
+    for(i = 0;i<5;i++)
+        {if (key == x[i])
+        {cout << "Your number found at: " << i+1;
+        break;
+        }
+        }
+        if (i==5)
+        cout<<"Number not found";
+    return 0;
+}
+
+/*
+Enter the number to find: 67
+Number not found
+
+Enter the number to find: 20
+Your number found at: 5
+*/
