bcec4adec613085ecd49e3b43307e57073ec6b76
akshitchordia <chordiaakshit3@gmail.com>
2025-08-03 21:11:57 +0530

    Create Printing String.cpp

---
+++ Printing String.cpp
@@ -0,0 +1,19 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    cout<<"Enter a string\n";
+    cin>>str1;
+    cout<<"Entered string is "<<str1;
+    return 0;
+}
+
+// OUTPUT
+// Enter a string
+// akshit
+// Entered string is akshit
